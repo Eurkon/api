f430b480bb073cd7663220de811f6aafd86d2e94
Eurkon <1124724692@qq.com>
2021-06-05 20:25:24 +0800

    feat: 根据不同的请求执行不同的方法

--- api/index.py
+++ api/index.py
@@ -2,53 +2,29 @@
 # @Author : Eurkon
 # @Date : 2021/6/3 16:55
 
-import requests
 import json
-from bs4 import BeautifulSoup
+from urllib.parse import parse_qs
 from http.server import BaseHTTPRequestHandler
 
-
-def get_data():
-    """爬取微博热搜
-
-    Args:
-
-
-    Returns:
-        微博热搜 json {title：标题, url：地址, num：数值, hot：等级}
-    """
-    
-    news = []
-    top_url = 'https://s.weibo.com/top/summary/'
-    r = requests.get(top_url)
-
-    soup = BeautifulSoup(r.text, 'html.parser')
-    url = soup.select('#pl_top_realtimehot > table > tbody > tr > td.td-02 > a')
-    num = soup.select('#pl_top_realtimehot > table > tbody > tr > td.td-02 > span')
-    hot = soup.select('#pl_top_realtimehot > table > tbody > tr > td.td-03')
-
-    for i in range(1, len(url)):
-        top_news = {}
-        top_news['title'] = url[i].get_text()
-        top_news['url'] = "https://s.weibo.com" + url[i]['href']
-        top_news['num'] = num[i - 1].get_text()
-        hotness = hot[i].get_text().replace('</i>', ''). \
-            replace('<i class="icon-txt icon-txt-hot">', '').replace('<i class="icon-txt icon-txt-new">', '')
-        top_news['hot'] = hotness
-        # 去除广告链接
-        if (hotness != '荐'):
-            news.append(top_news)
-
-    return news
+from api.weibo.api import *
+from api.weibo.api import *
 
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        data = get_data()
+
+        try:
+            params = parse_qs(self.path.split('?')[1])
+            api = params['api']
+            del params['api']
+            data = eval("{0}".format(api))(params)
+        except Exception as e:
+            data = json.dumps({'error': str(e)})
+
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Cache-Control', 'no-cache')
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(json.dumps(data).encode('utf-8'))
+        self.wfile.write(data)
         return
