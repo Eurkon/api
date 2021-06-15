3307e34d769e239b35487f5fb2f35320a2d87495
Eurkon <1124724692@qq.com>
2021-06-15 16:00:42 +0800

    feat: 增加图片返回

--- api/index.py
+++ api/index.py
@@ -15,64 +15,28 @@ from api.weibo.api.api import *
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        path = str(urlparse(self.path))
-        print(path)
-        params = dict(parse.parse_qsl(urlparse(self.path).query))
-        if len(params) > 0:
-            try:
-                api = params['api']
+        try:
+            params = dict(parse.parse_qsl(urlparse(self.path).query))
+            if 'api' in params:
+                api = str(params['api'])
                 del params['api']
                 data = eval("{0}".format(api))(params)
-            except Exception as e:
-                data = json.dumps({'error': e}).encode('utf-8')
-
-            if isinstance(data, bytes):
-                self.send_response(200)
-                self.send_header('Content-type', 'application/json')
-
-            elif isinstance(data, str):
-                self.send_response(302)
-                self.send_header('Location', data)
-                if data.endswith('.png'):
-                    self.send_header('Content-type', 'image/png')
-                elif data.endswith('.jpg'):
-                    self.send_header('Content-type', 'image/jpeg')
-                elif data.endswith('.html'):
-                    self.send_header('Content-type', 'text/html')
-                elif data.endswith('.xml'):
-                    self.send_header('Content-type', 'application/xml')
-                elif data.endswith('.pdf'):
-                    self.send_header('Content-type', 'application/pdf')
-                elif data.endswith('.txt'):
-                    self.send_header('Content-type', 'application/octet-stream')
-                else:
-                    self.send_header('Content-type', 'text/plain')
             else:
-                self.send_header('Content-type', 'text/plain')
+                data = {'error': '请输入API'}
+        except Exception as e:
+            data = {'error': str(e)}
 
-            self.send_header('Access-Control-Allow-Origin', '*')
-            self.send_header('Cache-Control', 'no-cache')
-            self.end_headers()
-            if isinstance(data, bytes):
-                self.wfile.write(data)
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Cache-Control', 'no-cache')
+        if isinstance(data, bytes):
+            res = data
+        elif isinstance(data, dict) or isinstance(data, list):
+            res = json.dumps(data).encode('utf-8')
         else:
+            res = str(data).encode('urf-8')
 
-            if path.endswith('.png'):
-                self.send_header('Content-type', 'image/png')
-            elif path.endswith('.jpg'):
-                self.send_header('Content-type', 'image/jpeg')
-            elif path.endswith('.html'):
-                self.send_header('Content-type', 'text/html')
-            elif path.endswith('.xml'):
-                self.send_header('Content-type', 'application/xml')
-            elif path.endswith('.pdf'):
-                self.send_header('Content-type', 'application/pdf')
-            elif path.endswith('.txt'):
-                self.send_header('Content-type', 'application/octet-stream')
-            else:
-                self.send_header('Content-type', 'text/plain')
-            self.send_response(200)
-            self.send_header('Access-Control-Allow-Origin', '*')
-            self.send_header('Cache-Control', 'no-cache')
-            self.end_headers()
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(res)
         return
