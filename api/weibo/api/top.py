aafe2f50b028c4ff9af673e02cbbec04df0c20ce
Eurkon <1124724692@qq.com>
2021-09-28 14:12:02 +0800

    fix: 修改“沸”的判断规则

--- api/weibo/api/top.py
+++ api/weibo/api/top.py
@@ -42,7 +42,7 @@ def weibo_top(params):
             hot = '爆'
         if 'is_hot' in data_item:
             hot = '热'
-        if 'is_boil' in data_item:
+        if 'is_fei' in data_item:
             hot = '沸'
         if 'is_new' in data_item:
             hot = '新'
