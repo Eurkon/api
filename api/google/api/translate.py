9d065b2d63654e25805e23b0dcbe0959d80d367e
Eurkon <1124724692@qq.com>
2021-11-09 11:58:44 +0800

    refactor: 谷歌翻译

--- api/google/api/translate.py
+++ api/google/api/translate.py
@@ -6,50 +6,6 @@ import execjs
 import requests
 
 
-class Py4Js():
-    def __init__(self):
-        self.ctx = execjs.compile("""
-        function TL(a) {
-        var k = "";
-        var b = 406644;
-        var b1 = 3293161072;
-        var jd = ".";
-        var $b = "+-a^+6";
-        var Zb = "+-3^+b+-f";
-        for (var e = [], f = 0, g = 0; g < a.length; g++) {
-            var m = a.charCodeAt(g);
-            128 > m ? e[f++] = m : (2048 > m ? e[f++] = m >> 6 | 192 : (55296 == (m & 64512) && g + 1 < a.length && 56320 == (a.charCodeAt(g + 1) & 64512) ? (m = 65536 + ((m & 1023) << 10) + (a.charCodeAt(++g) & 1023),
-            e[f++] = m >> 18 | 240,
-            e[f++] = m >> 12 & 63 | 128) : e[f++] = m >> 12 | 224,
-            e[f++] = m >> 6 & 63 | 128),
-            e[f++] = m & 63 | 128)
-        }
-        a = b;
-        for (f = 0; f < e.length; f++) a += e[f],
-        a = RL(a, $b);
-        a = RL(a, Zb);
-        a ^= b1 || 0;
-        0 > a && (a = (a & 2147483647) + 2147483648);
-        a %= 1E6;
-        return a.toString() + jd + (a ^ b)
-    };
-    function RL(a, b) {
-        var t = "a";
-        var Yb = "+";
-        for (var c = 0; c < b.length - 2; c += 3) {
-            var d = b.charAt(c + 2),
-            d = d >= t ? d.charCodeAt(0) - 87 : Number(d),
-            d = b.charAt(c + 1) == Yb ? a >>> d: a << d;
-            a = b.charAt(c) == Yb ? a + d & 4294967295 : a ^ d
-        }
-        return a
-    }
-    """)
-
-    def get_tk(self, text):
-        return self.ctx.call("TL", text)
-
-
 def read_lang(lang):
     lang_dict = {'中文': 'zh-CN', '阿尔巴尼亚语': 'sq', '阿拉伯语': 'ar',
                  '阿姆哈拉语': 'am', '阿塞拜疆语': 'az', '爱尔兰语': 'ga',
@@ -108,8 +64,12 @@ def google_translate(params):
     Returns:
         json: {result: 翻译后的内容}
     """
-    js = Py4Js()
-    tk = js.get_tk(str(params['content']))
+
+    with open('./translate.js', 'r', encoding='UTF-8') as file:
+        js_text = file.read()
+        # 编译加载js字符串
+        js = execjs.compile(js_text)
+        tk = js.call("TL", str(params['content']))
     return from_to(read_lang(params['from']), read_lang(params['to']), params['content'], tk)
 
 
